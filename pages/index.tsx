285e06027f12c78d28ce3acf9d92b46e26abb146
yusangKim <yoo99485@likelion.net>
2022-04-11 12:21:17 +0900

    [Add[ Basic Example

---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className="p-10">
+      <Head>
+        <title>react-table</title>
+      </Head>
+      {/* Body */}
+      <div className="text-center">Example List</div>
+      <div className="flex justify-between border-2 mt-10 p-10">
+        <div className="w-2/4 ">
+          <ul className="flex flex-col gap-10">
+            <li onClick={() => router.push('/example/basic')}>Basic</li>
+            <li>Footers</li>
+            <li>Sorting</li>
+            <li>Filtering</li>
+            <li>Grouping</li>
+            <li>Grouping Column</li>
+            <li>Pagination</li>
+            <li>Row Selection</li>
+          </ul>
+        </div>
+        <div className="w-2/4 ">
+          <ul className="flex flex-col gap-10">
+            <li>Row Selection With Pagination</li>
+            <li>Expanding</li>
+            <li>Sub Components</li>
+            <li>Sub Components(lazy)</li>
+            <li>Editable Data</li>
+            <li>Column Ordering</li>
+            <li>Column Hiding</li>
+            <li>Column Resizting</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
